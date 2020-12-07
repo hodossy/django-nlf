649582dc5c5afbbed46632a2ed6346aa66b6dda2
Hodossy, Szabolcs <hodossy.szabolcs@gmail.com>
2020-12-07 11:30:31 +0100

    chore: fix documentation

--- docs/conf.py
+++ docs/conf.py
@@ -30,11 +30,10 @@ author = 'Hodossy, Szabolcs'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinx_rtd_theme'
+    'sphinx.ext.intersphinx'
 ]
 
-html_theme = 'sphinx_rtd_theme'
+autodoc_member_order = 'groupwise'
 
 # Add any inventories here that are referenced.
 intersphinx_mapping = {
@@ -56,7 +55,23 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+
+html_theme_options = {
+    'canonical_url': '',
+    'analytics_id': '',
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
