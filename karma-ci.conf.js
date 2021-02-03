ae2df1ca84327e169daffd876a8bc241ddd9c7a7
Hodossy, Szabolcs <hodossy.szabolcs@gmail.com>
2021-02-03 11:56:26 +0100

    chore: add coverage measurement to js tests

--- karma-ci.conf.js
+++ karma-ci.conf.js
@@ -6,12 +6,22 @@ module.exports = function(config) {
       "django_nlf/static/django_nlf/js/autocomplete.js",
       "tests/javascripts/**/*.spec.js",
     ],
+    preprocessors: {
+      "django_nlf/**/*.js": ["coverage"]
+    },
     frameworks: ["jasmine"],
     browsers: ["ChromeCI"],
-    reporters: ["dots"],
+    reporters: ["dots", "coverage"],
+    coverageReporter: {
+      reporters: [
+        { type: "text" },
+        { type: "text-summary" },
+      ]
+    },
     plugins: [
       "karma-chrome-launcher",
       "karma-jasmine",
+      "karma-coverage",
     ],
     customLaunchers: {
       ChromeCI: {
